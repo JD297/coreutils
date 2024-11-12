3ac57196778d7f16d6fe165b57d09a96daaa8c12
JD297 <jan.dommasch297@gmail.com>
2024-11-12 20:44:55 +0100

    Added -l option - ls

--- src/ls.c
+++ src/ls.c
@@ -2,9 +2,35 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main()
+void print_usage()
 {
+	printf("Usage: %s [OPTION]...\n", TARGET);
+	printf("List stats from files\n");
+	printf("\n");
+	printf("  -l		List files vertically\n");
+	printf("\n");
+	printf("JD297 coreutils source code <https://github.com/jd297/coreutils>\n");
+}
+
+int main(int argc, char** argv)
+{
+	int lflag = 0;
+
+	int opt;
+
+	while ((opt = getopt(argc, argv, "l")) != -1) {
+		switch (opt) {
+			case 'l':
+				lflag = 1;
+				break;
+			default:
+				print_usage();
+				exit(EXIT_FAILURE);
+		}
+	}
+
 	const char* path = ".";
 
 	DIR *d;
@@ -15,12 +41,40 @@ int main()
 		err(EXIT_FAILURE, "%s", path);
 	}
 
+	char** list;
+
+	list = (char**)calloc(1, sizeof(char*));
+
+	int list_index = 0;
+
+	if (list == NULL) {
+		err(EXIT_FAILURE, "calloc");
+	}
+
 	while ((dir = readdir(d)) != NULL) {
-		printf("%s\n", dir->d_name);
+		list = realloc(list, (list_index + 1) * sizeof(char*));
+
+		if (list == NULL) {
+			err(EXIT_FAILURE, "realloc");
+		}
+
+		list[list_index] = dir->d_name;
+
+		list_index++;
 	}
 
 	if (closedir(d) == -1) {
 		err(EXIT_FAILURE, "%s", path);
+	}
+
+	for (int i = 0; i < list_index; i++) {
+		printf("%s", list[i]);
+
+		if (lflag || list_index - 1 == i) {
+			printf("\n");
+		} else {
+			printf("  ");
+		}
 	}
 
 	return 0;
