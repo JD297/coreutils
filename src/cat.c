adc780b0a4616f9e2d039be8239736251664ba03
JD297 <jan.dommasch297@gmail.com>
2024-11-21 22:09:14 +0100

    Added error handling for directory - cat

--- src/cat.c
+++ src/cat.c
@@ -15,7 +15,7 @@ void print_usage()
 	printf("JD297 %s source code <https://github.com/jd297/coreutils>\n", TARGET);
 }
 
-#define HANDLE_ERROR(object) fprintf(stderr, "%s: %s: %s\n", TARGET, object, strerror(errno)); \
+#define HANDLE_ERROR(object, error) fprintf(stderr, "%s: %s: %s\n", TARGET, object, strerror(error)); \
                              continue
 
 int main(int argc, char** argv)
@@ -30,31 +30,35 @@ int main(int argc, char** argv)
 		struct stat sb;
 
 		if (stat(argv[i], &sb) == -1) {
-			HANDLE_ERROR(argv[i]);
+			HANDLE_ERROR(argv[i], errno);
+		}
+
+		if ((sb.st_mode & S_IFMT) == S_IFDIR) {
+			HANDLE_ERROR(argv[i], EISDIR);
 		}
 
 		int fd;
 
 		if ((fd = open(argv[i], O_RDONLY)) == -1) {
-			HANDLE_ERROR(argv[i]);
+			HANDLE_ERROR(argv[i], errno);
 		}
 
 		char *buf = (char *)mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
 		if (buf == MAP_FAILED) {
-			HANDLE_ERROR("mmap");
+			HANDLE_ERROR("mmap", errno);
 		}
 
 		if (write(STDOUT_FILENO, buf, sb.st_size) == -1) {
-			HANDLE_ERROR("write");
+			HANDLE_ERROR("write", errno);
 		}
 
 		if (munmap(buf, sb.st_size) == -1) {
-			HANDLE_ERROR("munmap");
+			HANDLE_ERROR("munmap", errno);
 		}
 
 		if (close(fd) == -1) {
-			HANDLE_ERROR(argv[i]);
+			HANDLE_ERROR(argv[i], errno);
 		}
 	}
 
