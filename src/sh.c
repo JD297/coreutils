448a59658d6c8543a1a31f4894eb84753f84b463
JD297 <jan.dommasch297@gmail.com>
2024-11-25 16:33:12 +0100

    Fixed simpler calls for JD297/libc - sh

--- src/sh.c
+++ src/sh.c
@@ -1,22 +1,24 @@
 #include <err.h>
+#include <errno.h>
 #include <linux/limits.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int sh_cd(int argc, char** argv)
 {
 	if (argc != 2) {
-		printf("%s: Usage: %s [dir]\n", TARGET, argv[0]);
-		return 1;
+		fprintf(stderr, "%s: Usage: %s [dir]\n", TARGET, argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
 	int ret;
 
 	if ((ret = chdir(argv[1])) == -1) {
-		perror("chdir");
+		err(EXIT_FAILURE, "chdir");
 	}
 
 	return ret;
@@ -24,12 +26,26 @@ int sh_cd(int argc, char** argv)
 
 int main()
 {
-	char* command;
+	char *command;
 	size_t len = 0;
 	ssize_t nread;
 
+	size_t arguments_size = 4096 * 2; // TODO get pagesize
+
+	char** arguments = (char**)mmap(NULL, arguments_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, NULL, 0);
+
+	if (arguments == MAP_FAILED) {
+		err(EXIT_FAILURE, "mmap (arguments)");
+	}
+
+	size_t command_size = 4096 * 2; // TODO get pagesize
+
+	if ((command = (char *)mmap(NULL, command_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, NULL, 0)) == MAP_FAILED) {
+		err(EXIT_FAILURE, "mmap (command)");
+	}
+
 	do {
-		command = NULL;
+		command = memset(command, 0, command_size);
 
 		char cwd[PATH_MAX];
 
@@ -39,9 +55,8 @@ int main()
 
 		printf("%s# ", cwd);
 
-		if ((nread = getline(&command, &len, stdin)) == -1) {
-			free(command);
-			err(EXIT_FAILURE, "%s", "getline");
+		if ((nread = read(STDIN_FILENO, command, command_size)) == -1) {
+			err(EXIT_FAILURE, "read (command)");
 		}
 
 		if (nread == 0 || nread == 1) {
@@ -52,53 +67,49 @@ int main()
 			command[nread-1] = '\0';
 		}
 
-		char** arguments;
-
-		arguments = (char**)calloc(1, sizeof(char*));
-
-		if (arguments == NULL) {
-			err(EXIT_FAILURE, "calloc");
-		}
+		arguments = memset(arguments, 0, arguments_size);
 
 		int arguments_index = 0;
 
-		arguments[arguments_index] = strtok(command, " ");
+		char *saveptr;
+
+		arguments[arguments_index] = strtok_r(command, " ", &saveptr);
 
 		#define program arguments[0]
 
 		if (program == NULL) {
-			free(arguments);
 			continue;
 		}
 
 		while (arguments[arguments_index] != NULL) {
 			arguments_index++;
 
-			arguments = realloc(arguments, (arguments_index + 1) * sizeof(char*));
+			if (arguments_size < arguments_index * sizeof(char *)) {
+				arguments = (char **)mremap(arguments, arguments_size, arguments_size * 2, MREMAP_MAYMOVE | MREMAP_DONTUNMAP);
+				arguments_size = arguments_size * 2;
 
-			if (arguments == NULL) {
-				err(EXIT_FAILURE, "realloc");
+				if (arguments == MAP_FAILED) {
+					err(EXIT_FAILURE, "mremap (arguments)");
+				}
 			}
 
-			arguments[arguments_index] = strtok(NULL, " ");
+			arguments[arguments_index] = strtok_r(NULL, " ", &saveptr);
 		}
 
 		if (strcmp(program, "exit") == 0) {
-			free(arguments);
 			break;
 		}
 
 		if (strcmp(program, "cd") == 0) {
 			sh_cd(arguments_index, arguments);
 
-			free(arguments);
 			continue;
 		}
 
 		pid_t pid;
 
 		if ((pid = fork()) == -1) {
-			perror("fork");
+			fprintf(stderr, "%s: fork: ", strerror(errno));
 			continue;
 		}
 
@@ -111,11 +122,15 @@ int main()
 		}
 
 		wait(NULL);
-
-		free(arguments);
 	} while (1);
 
-	free(command);
+	if (munmap(arguments, arguments_size) == -1) {
+		err(EXIT_FAILURE, "munmap (arguments)");
+	}
+
+	if (munmap(command, command_size) == -1) {
+		err(EXIT_FAILURE, "munmap (command)");
+	}
 
 	return 0;
 }
